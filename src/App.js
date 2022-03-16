508393f666667af2372d5013599664f9719890e3
Ahmedelabasyy <ahmedelabasy662@gmail.com>
2022-03-16 15:57:51 +0200

    Edition

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
-import PageNotFound from "./components/PageNotFound/PageNotFound";
 import MovieDetail from "./components/MovieDetail/MovieDetail";
 import './App.scss';
 
@@ -16,7 +15,6 @@ function App() {
                         <Routes >
                                 <Route path="/" element={<Home />} />
                                 <Route path="/movie/:imdbID" element={<MovieDetail />} />
-                                <Route path='*' element={<PageNotFound />} />
                         </Routes>
                     </div>
                 <Footer />
